794c9736acbd7f8bb6553ac8f0e98ff0faccc216
Ernie Miller <ernie@metautonomo.us>
2010-04-30 10:43:04 -0400

    Add eql? and hash methods to MetaWhere::Column for mergability

--- lib/meta_where/column.rb
+++ lib/meta_where/column.rb
@@ -4,11 +4,21 @@ module MetaWhere
     
     def initialize(column, method)
       @column = column.to_s
-      @method = method
+      @method = method.to_s
     end
     
     def %(value)
       MetaWhere::Condition.new(column, value, method)
+    end
+    
+    def eql?(other_column)
+      other_column.is_a?(Column)    &&
+      column == other_column.column &&
+      method == other_column.method
+    end
+    
+    def hash
+      (column + '#' + method).hash
     end
     
     # Play "nicely" with expand_hash_conditions_for_aggregates
