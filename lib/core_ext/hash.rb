d46601a80a173cbe2a78c3625b77aed86068ad0e
Ernie Miller <ernie@metautonomo.us>
2010-09-29 20:16:09 -0400

    Arel 2.0 compatability

--- lib/core_ext/hash.rb
+++ lib/core_ext/hash.rb
@@ -1,16 +1,22 @@
 class Hash
   def to_predicate(builder, parent = nil)
-    Arel::Predicates::All.new(*builder.build_predicates_from_hash(self, parent || builder.join_dependency.join_base))
+    predicates = builder.build_predicates_from_hash(self, parent || builder.join_dependency.join_base)
+    if predicates.size > 1
+      first = predicates.shift
+      Arel::Nodes::Grouping.new(predicates.inject(first) {|memo, expr| Arel::Nodes::And.new(memo, expr)})
+    else
+      predicates.first
+    end
   end
-  
+
   def to_attribute(builder, parent = nil)
     builder.build_attributes_from_hash(self, parent)
   end
-  
+
   def |(other)
     MetaWhere::Or.new(self, other)
   end
-  
+
   def &(other)
     MetaWhere::And.new(self, other)
   end
--- lib/meta_where.rb
+++ lib/meta_where.rb
@@ -2,11 +2,25 @@ module MetaWhere
   METHOD_ALIASES = {
     'ne' => :not_eq,
     'like' => :matches,
-    'nlike' => :not_matches,
+    'not_matches' => :does_not_match,
+    'nlike' => :does_not_match,
     'lte' => :lteq,
     'gte' => :gteq,
     'nin' => :not_in
   }
+
+  PREDICATES = [
+    :eq, :eq_any, :eq_all,
+    :not_eq, :not_eq_any, :not_eq_all,
+    :matches, :matches_any, :matches_all,
+    :does_not_match, :does_not_match_any, :does_not_match_all,
+    :lt, :lt_any, :lt_all,
+    :lteq, :lteq_any, :lteq_all,
+    :gt, :gt_any, :gt_all,
+    :gteq, :gteq_any, :gteq_all,
+    :in, :in_any, :in_all,
+    :not_in, :not_in_any, :not_in_all
+  ]
 
   def self.operator_overload!
     require 'core_ext/symbol_operators'
