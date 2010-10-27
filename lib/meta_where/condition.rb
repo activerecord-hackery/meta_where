e4cb80712c992f737dabec2b0bcc813319f3fd9c
Ernie Miller <ernie@metautonomo.us>
2010-10-27 08:14:31 -0400

    Fix typo in MetaWhere::Condition#==

--- lib/meta_where/condition.rb
+++ lib/meta_where/condition.rb
@@ -3,18 +3,18 @@ require 'meta_where/utility'
 module MetaWhere
   class Condition
     include MetaWhere::Utility
-    
+
     attr_reader :column, :value, :method
-    
+
     def initialize(column, value, method)
       @column = column.to_s
       @value = value
       @method = (MetaWhere::METHOD_ALIASES[method.to_s] || method).to_s
     end
-    
+
     def to_predicate(builder, parent = nil)
       table = builder.build_table(parent)
-      
+
       unless attribute = table[column]
         raise ::ActiveRecord::StatementInvalid, "No attribute named `#{column}` exists for table `#{table.name}`"
       end
@@ -24,28 +24,28 @@ module MetaWhere
       end
       attribute.send(method, *args_for_predicate(method.to_s, value))
     end
-    
+
     def ==(other_condition)
       other_condition.is_a?(Condition) &&
       other_condition.column == column &&
-      other_condition.value = value    &&
+      other_condition.value == value    &&
       other_condition.method == method
     end
-    
+
     alias_method :eql?, :==
-    
+
     def |(other)
       Or.new(self, other)
     end
-    
+
     def &(other)
       And.new(self, other)
     end
-    
+
     # Play "nicely" with expand_hash_conditions_for_aggregates
     def to_sym
       self
     end
   end
-  
+
 end
